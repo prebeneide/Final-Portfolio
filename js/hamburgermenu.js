024e6a019067e0711832c797c30d85880dc290bf
prebeneide <71383450+prebeneide@users.noreply.github.com>
2022-03-24 17:12:41 +0100

    first step with hamburger

---
+++ js/hamburgermenu.js
@@ -0,0 +1,27 @@
+function navAnimation() {
+
+    const links = document.querySelectorAll(".nav-links li");
+
+    const list = document.querySelector("nav-links");
+
+    const menu = document.querySelector(".menuicon");
+
+    menu.addEventListener("click", function() {
+
+        list.classList.toggle("navtransform");
+
+        links.forEach((item, number) => {
+            if (item.style.animation) {
+                item.style.animation = ""
+                document.body.style.overflow = "initial";
+            } else {
+                item.style.animation = `fadingIn 0.6s ease forwards ${number / 6 + 0.4}s`;
+                document.body.style.overflow = "hidden";
+            }
+        });
+
+        menu.classList.toggle("menuiconchange");
+    })
+}
+
+navAnimation();
